2e74570dcc61e136e45417ce7359e71d0f786a0c
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-06-10 22:04:15 -0300

    Create blog home page

--- personal_portfolio_project/blog/models.py
+++ personal_portfolio_project/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateField(default=date.today)
+    description = models.CharField(max_length=250)
+    url = models.URLField(default="https://google.com")
+
+    def __str__(self):
+        return self.title
--- personal_portfolio_project/blog/views.py
+++ personal_portfolio_project/blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+# def all_blogs(request):
+#     return render(request, 'blog/all_blogs.html')
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
