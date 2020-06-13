15863f021a3cf0c8cf9ded1f819b060ffbb07ef7
Mr-Saxobeat <weiglasribeiro@gmail.com>
2020-06-12 21:34:52 -0300

    Create todo project

---
+++ todowoo_project/todo/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        # Create a new user
